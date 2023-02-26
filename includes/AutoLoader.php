e589707e90386cf9f5610441700f438f43c55e5a
NATASHA-ct <chirombenatashatatenda@gmail.com>
2023-02-26 10:12:25 +0200

    Add AUTOLOADER

---
+++ includes/AutoLoader.php
@@ -0,0 +1,15 @@
+<?php
+
+spl_autoload_register('myAutoLoader');
+
+function myAutoLoader($className){
+	$path = 'classes/';
+	$extension = '.class.php';
+	$fileName = $path . $className . $extension;
+
+    if (!file_exists($fileName)) {
+        return false;
+    }
+
+require_once $path . $className . $extension;
+}
