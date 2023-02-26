185ac766b931c21f021b8f747d6c92293c0ad430
NATASHA-ct <chirombenatashatatenda@gmail.com>
2023-02-26 10:26:56 +0200

    Add methods to grab data from user wen login

--- classes/login.classes.php
+++ classes/login.classes.php
@@ -19,15 +19,19 @@
 					exit();
 				}
 				
+                // CHECK IF WE ARE GWTTING ANYTHING FROM DB
 				if ($stmt->rowCount() == 0) {
 					$stmt=null;
 					header("location: ../index.php?error=usernotfound");
 					exit();
 				}
 			
+
+                // PDO::FETCH_ASSOC - ITS US TELLING THE DB WE NEED Data to be fetched and returned in an associated array
 				$pwdHashed = $stmt->fetchAll(PDO::FETCH_ASSOC);
 				$checkPwd = password_verify($pwd, $pwdHashed[0]["users_pwd"]);
-			
+
+            // check if pwd is the same as in the DB
 			if ($checkPwd == false) {
 				
 				$stmt=null;
@@ -37,13 +41,17 @@
 			else{
 				
 				$stmt = $this->connect()->prepare('SELECT * FROM users WHERE users_uid = ? OR users_email = ? AND users_pwd = ?;');
+
+                // checking for errors  in the DB
 				if (!$stmt->execute(array($uid, $uid, $pwdHashed))){
 					
 					$stmt=null;
 					header("location: ../index.php?error=stmtfailed");
 					exit();
 				}
-				
+
+
+				// CHECK IF WE ARE GWTTING ANYTHING FROM DB
 				if ($stmt->rowCount() == 0) {
 					
 					$stmt=null;
@@ -51,10 +59,13 @@
 					exit();
 				}
 				
+
+                // PDO::FETCH_ASSOC - ITS US TELLING THE DB WE NEED Data to be fetched and returned in an associated array
 				$user = $stmt->fetchAll(PDO::FETCH_ASSOC);
 				
 				session_start();
 				
+                // grab data from DB
 				$_SESSION["userid"] = $user[0]["users_id"];
 				$_SESSION["useruid"] = $user[0]["users_uid"];
 				$stmt=null;
--- includes/login.inc.php
+++ includes/login.inc.php
@@ -1,2 +1,22 @@
 <!-- file receiving data from the user from browser -->
 <?php
+	require_once '../classes/login-contr.classes.php';
+	require_once '../classes/login.classes.php';
+	require_once '../classes/dbh.classes.php';
+	
+	if(isset($_POST["submit"])){
+		
+		// Grabbing the data
+		$uid = $_POST["uid"];
+		$pwd = $_POST["pwd"];
+		
+		// Instantiate SignupController class
+		$loginController = new LoginController($uid, $pwd);
+		
+		
+		// Running error handlers and signup
+		$loginController->loginUser();
+		
+		// Going back to front page
+		header("location: ../index.php?error=loginnone");
+	}
